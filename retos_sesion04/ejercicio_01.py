bd715cdbbc2f2e1ce43f8d72b5f411dca49672d6
Daron Baldiviezo <daron.augusto@gmail.com>
2024-05-01 21:59:50 -0400

    Add print square

---
+++ retos_sesion04/ejercicio_01.py
@@ -0,0 +1,6 @@
+# The porpouse of this exercise is only to print a square with multiple lines of print
+print("=========")
+print("|       |")
+print("|       |")
+print("|       |")
+print("=========")
