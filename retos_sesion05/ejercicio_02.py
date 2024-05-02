9c8865942279666ef926449d93f4666bc345eca2
Daron Baldiviezo <daron.augusto@gmail.com>
2024-05-02 00:24:03 -0400

    conversor de grados

---
+++ retos_sesion05/ejercicio_02.py
@@ -0,0 +1,9 @@
+def farenh(celsius):
+    farenheit = ((celsius * 9) / 5) + 32
+    print("Celsius: ", celsius, "      Farenheit: ", farenheit)
+
+farenh(30)
+farenh(-273.99)
+farenh(100)
+
+# Perdon si uso la funcion, se que aun no tocamos el tema pero me emocione jeje
