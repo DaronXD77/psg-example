deccdda69be2908f7572049b1a792777347c8052
Daron Baldiviezo <daron.augusto@gmail.com>
2024-05-02 00:30:06 -0400

    conversor de segundos

---
+++ retos_sesion05/ejercicio_03.py
@@ -0,0 +1,8 @@
+seg = 288325
+dia = ((seg / 60) / 60) / 24
+hor = (seg / 60) / 60
+min = seg / 60
+print("Dias: ", int(dia))
+print("Horas: ", hor)
+print("Minutos: ", min)
+print("Segundos: ", seg)
